7f8e83ca66f19b917010b4cd94eb839f015acd22
Casper Chen <cdarkz@ms.usi.com.tw>
2015-12-25 11:36:15 +0800

    USBIP: modify to use defined IP

--- usbip_scripts/test_ap_usbip.sh
+++ usbip_scripts/test_ap_usbip.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
+TFTPD_IP=192.168.1.254
+
 cd /tmp
 killall usbipd
 rmmod usbip_host usbip_core
-tftp -gr usbip-core.ko 192.168.1.254
-tftp -gr usbip-host.ko 192.168.1.254
-tftp -gr libusbip.so.0.0.1 192.168.1.254
+tftp -gr usbip-core.ko $TFTPD_IP
+tftp -gr usbip-host.ko $TFTPD_IP
+tftp -gr libusbip.so.0.0.1 $TFTPD_IP
 ln -sf libusbip.so.0.0.1 libusbip.so.0
 ln -sf libusbip.so.0.0.1 libusbip.so
-tftp -gr usbip 192.168.1.254
-tftp -gr usbipd 192.168.1.254
+tftp -gr usbip $TFTPD_IP
+tftp -gr usbipd $TFTPD_IP
 chmod a+x usbip usbipd
 insmod ./usbip-core.ko
 insmod ./usbip-host.ko
