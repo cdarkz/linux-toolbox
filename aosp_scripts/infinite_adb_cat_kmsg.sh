bd1d2609797e9f223ea875354661b806251b2934
Casper Chen <cdarkz@ms.usi.com.tw>
2016-05-10 15:12:21 +0800

    AOSP: let script exit when error happens

--- aosp_scripts/infinite_adb_cat_kmsg.sh
+++ aosp_scripts/infinite_adb_cat_kmsg.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+adb wait-for-device
 adb root
 adb wait-for-device
 
 while [ 1 ]
 do
 	echo "=======loop"
-	adb shell cat /proc/kmsg
-	#adb wait-for-device
+	adb shell cat /proc/kmsg || exit 1
 done
