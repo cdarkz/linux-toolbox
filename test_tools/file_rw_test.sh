aea9d5f32ab05ca0d41c798c6d84f661d65f1023
Casper Chen <cdarkz@ms.usi.com.tw>
2016-08-18 16:55:30 +0800

    TEST: add the parameter of number of files for r/w test

--- test_tools/file_rw_test.sh
+++ test_tools/file_rw_test.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
-# $1: times for test
+# $1: repeat times for test
 # $2: size for r/w file (KB)
-# $3: path for test
+# $3: number of files to do test
+# $4: path for test
 
-if [ $1 ]; then
-	TESTCOUNT=$1
-else
-	TESTCOUNT=10
+function usage() {
+	echo "$1 <repeat> <size> <number of files> <path>"
+}
+
+if [ $# -ne 4 ]; then
+	usage $0
+	exit 1
 fi
 
-if [ $2 ]; then
-	TESTSIZE=$2
-else
-	TESTSIZE=1024
-fi
-
-if [ $3 ]; then
-	TESTPATH=$3
-else
-	TESTPATH=/tmp
-fi
+TESTCOUNT=$1
+TESTSIZE=$2
+TESTNUMBER=$3
+TESTPATH=$4
 
 SRCFILE=/tmp/src_test.file
 DSTFILE=${TESTPATH}/dst_test.file
 
 echo "R/W size ${TESTSIZE} for ${TESTCOUNT} tests on ${TESTPATH}"
+# check parameter
 if [ ! -d ${TESTPATH} ]; then
 	echo "\"${TESTPATH}\" doesn't exist"
 fi
@@ -35,23 +33,31 @@ fi
 count=1
 while [ ${count} -le ${TESTCOUNT} ]
 do
-	dd if=/dev/urandom of=${SRCFILE} bs=1K count=${TESTSIZE}
-	sync
-	SRCMD5=`md5sum ${SRCFILE} | cut -d' ' -f1`
-	echo "SRC file \"${SRCFILE}\" md5sum ${SRCMD5}"
+	number=1
+	while [ ${number} -le ${TESTNUMBER} ]
+	do
+		RSRCFILE=${SRCFILE}.${number}
+		RDSTFILE=${DSTFILE}.${number}
+ 
+		dd if=/dev/urandom of=${RSRCFILE} bs=1K count=${TESTSIZE}
+		sync
+		SRCMD5=`md5sum ${RSRCFILE} | cut -d' ' -f1`
+		echo "SRC file \"${RSRCFILE}\" md5sum ${SRCMD5}"
 
-	cp -f ${SRCFILE} ${DSTFILE}
-	sync
-	DSTMD5=`md5sum ${DSTFILE} | cut -d' ' -f1`
-	echo "DST file \"${DSTFILE}\" md5sum ${DSTMD5}"
+		cp -f ${RSRCFILE} ${RDSTFILE}
+		sync
+		DSTMD5=`md5sum ${RDSTFILE} | cut -d' ' -f1`
+		echo "DST file \"${RDSTFILE}\" md5sum ${DSTMD5}"
 
+		if [ "${SRCMD5}"=="${DSTMD5}" ]; then
+			echo "${count}.${number}: SRC file and DST file are same"
+		else
+			echo "${count}.${number}: SRC file and DST file are different!!!"
+			exit 1
+		fi
 
-	if [ "$SRCMD5"=="$DSTMD5" ]; then
-		echo "${count}: SRC file and DST file are same"
-	else
-		echo "${count}: SRC file and DST file are different!!!"
-		exit 1
-	fi
+		number=$(( ${number} + 1 ))
+	done
 
 	count=$(( ${count} + 1 ))
 done
