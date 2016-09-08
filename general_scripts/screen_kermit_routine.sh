d749c23af979d7efb5f8b7b05b9f23b115d7f3b2
Casper Chen <cdarkz@ms.usi.com.tw>
2016-09-08 11:30:23 +0800

    GENERAL: fix screnn routine script for crontab use

--- general_scripts/screen_kermit_routine.sh
+++ general_scripts/screen_kermit_routine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for screenfull in "/var/run/screen/S-$USER"/*;
+user=`whoami`
+echo "screen for $user"
+for screenfull in "/var/run/screen/S-$user"/*;
 do
 	screenid=`basename ${screenfull} | cut -d. -f1`
 	if [ `cat /proc/${screenid}/cmdline | grep -c kermit` == '1' ]; then
@@ -9,8 +11,10 @@ do
 		screen -x ${screenid} -X stuff '\034\103'
 		screen -x ${screenid} -X stuff '\003'
 		screen -x ${screenid} -X stuff 'exit\015'
+		sleep 1
 	fi
 done
 
+sleep 3
 screen -d -m kermit -y ~/kermit_log/kermrc_usb
 screen -d -m kermit
